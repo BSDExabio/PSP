e750718baa95c123bfa25abc07a04a26800e4153
Mark Coletti <colettima@ornl.gov>
2021-08-30 10:40:13 -0400

    Set more scaffolding to read protein file and farm out to dask workers

--- src/alphataskmgr/alphafoldtaskmgr.py
+++ src/alphataskmgr/alphafoldtaskmgr.py
@@ -8,6 +8,8 @@
 import argparse
 import logging
 import csv
+from pathlib import Path
+from time import time, sleep
 
 from rich.console import Console
 from rich.table import Table
@@ -27,6 +29,7 @@ logging.basicConfig(level='DEBUG', format='%(message)s',
 
 from distributed import Client, LocalCluster
 
+
 def get_num_workers(client):
     """
     :param client: active dask client
@@ -35,6 +38,31 @@ def get_num_workers(client):
     scheduler_info = client.scheduler_info()
 
     return len(scheduler_info['workers'].keys())
+
+
+def read_input_file(input_file):
+    """ Read text file containing proteins to be processed
+
+    :param input_file: path to input file of proteins
+    :return: list of proteins to be processed
+    """
+    input_file_path = Path(input_file)
+    if not input_file_path.exists():
+        raise RuntimeError(f'{input_file} does not exist')
+
+    with input_file_path.open('r') as file_input:
+        return list(file_input.readlines())
+
+
+def run_alphafold(protein):
+    start_time = time()
+
+    # TODO swap in subprocess call to alphafold here
+    sleep(3)
+
+    stop_time = time()
+
+    return start_time, stop_time, protein
 
 
 if __name__ == '__main__':
@@ -58,6 +86,23 @@ if __name__ == '__main__':
 
         logging.info(f'Starting with {get_num_workers(client)} dask workers.')
 
-        logging.info(f'Finished with {get_num_workers(client)} dask workers still active.')
+
+        # Slurp in all the proteins we have to process
+        proteins = read_input_file(args.input_file)
+
+        logging.info(f'Read {len(proteins)} proteins to process.')
+
+        task_futures = client.map(run_alphafold, task_values)
+
+        ac = as_completed(task_futures)
+
+        for i, finished_task in enumerate(ac):
+            start_time, stop_time, protein = finished_task.result()
+
+            print(f'{protein} processed in '
+                  f'{(stop_time - start_time) / 60} minutes.')
+
+        logging.info(f'Finished with {get_num_workers(client)} dask workers '
+                     f'still active.')
 
     logging.info('Done.')
