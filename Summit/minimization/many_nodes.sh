46c86979b38fb2b5dd4cd50c522ec855e3a02185
Russell Davidson <davidsonrb@ornl.gov>
2021-11-17 11:17:47 -0500

    much work to implement and debug the dask pipeline; removed the necessity for nested bash scripts; dask pipeline and minimization functions are now within minimization_taskmgr.py script

--- Summit/minimization/many_nodes.sh
+++ Summit/minimization/many_nodes.sh
@@ -13,51 +13,72 @@
 #BSUB -o testing.%J.out
 #BSUB -e testing.%J.err
 
-RUN_DIR=/gpfs/alpine/proj-shared/bip198/minimize_af/WP_010940344.1/
+# set up the modules and python environment
+module load cuda/10.2.89 gcc/8.1.1
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/ccs/home/davidsonrb/Apps/miniconda_SUMMIT/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/ccs/home/davidsonrb/Apps/miniconda_SUMMIT/etc/profile.d/conda.sh" ]; then
+        . "/ccs/home/davidsonrb/Apps/miniconda_SUMMIT/etc/profile.d/conda.sh"
+    else
+        export PATH="/ccs/home/davidsonrb/Apps/miniconda_SUMMIT/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# activate the conda environment
+conda activate openmm
+
+# set active directories
+RUN_DIR=/gpfs/alpine/proj-shared/bip198/minimize_af/WP_010940344.1/test
 SCHEDULER_FILE=${RUN_DIR}/scheduler_file.json
 
-source /autofs/nccs-svm1_home1/davidsonrb/Scripts/debugging/af_minimization/common.env
-
-mkdir -p $RUN_DIR
+if [ ! -d "$SCHEDULER_DIR" ]
+then
+    mkdir -p $RUN_DIR
+fi
 cd $RUN_DIR
 
-# Copy over the hosts allocated for this job so that we can later verify
-# that all the allocated nodes were busy with the correct worker allocation.
-cat $LSB_DJOB_HOSTFILE | sort | uniq > $LSB_JOBID.hosts
+# Copy over the hosts allocated for this job so that we can later verify that all the allocated nodes were busy with the correct worker allocation.
+cat $LSB_DJOB_HOSTFILE | sort | uniq > $LSB_JOBID.hosts		# catches both the batch and compute nodes; not super interested in the batch node though, right?
 
 # We need to figure out the number of nodes to later spawn the workers
-NUM_NODES=$(cat $LSB_DJOB_HOSTFILE | sort | uniq | wc -l)
-export NUM_NODES=$(expr $NUM_NODES - 1)
+NUM_NODES=$(cat $LSB_JOBID.hosts | wc -l)	# count number of lines in $LSB_JOBID.hosts
+export NUM_NODES=$(expr $NUM_NODES - 1)		# subtract by one to ignore the batch node
 
 ##
-## Start dask scheduler on an arbitrary couple of CPUs (more than one CPU to
-## handle overhead of managing all the dask workers.)
+## Start dask scheduler on an arbitrary couple of CPUs (more than one CPU to handle overhead of managing all the dask workers).
 ##
 
-# The scheduler doesn't need GPUs. We give it two CPUs and 8 total threads to
-# handle the overhead of managing so many workers.
-jsrun --smpiargs="off" --gpu_per_rs 0 --nrs 1 --tasks_per_rs 1 --cpu_per_rs 2 --rs_per_host 1  --latency_priority cpu-cpu -b none dask-scheduler --interface ib0 --no-dashboard --no-show --scheduler-file $SCHEDULER_FILE &
+# The scheduler doesn't need GPUs. We give it 2 CPUs to handle the overhead of managing so many workers.
+jsrun --smpiargs="off" --nrs 1 --rs_per_host 1 --tasks_per_rs 1 --cpu_per_rs 2 --gpu_per_rs 0 --latency_priority cpu-cpu --bind none \
+	dask-scheduler --interface ib0 --no-dashboard --no-show --scheduler-file $SCHEDULER_FILE &
 
 # Give the scheduler a chance to spin up.
-sleep 5
+sleep 15
 
 ##
-## Start the dask-workers, which will be paired up to an individual GPU.  This
-## bash script will manage the dask workers and GPU allocation for each Summit node.
+## Start the dask-workers, which will be paired up to an individual GPU.  This bash script will manage the dask workers and GPU allocation for each Summit node.
 ##
 
-# Now launch ALL the dask workers simultaneously.  They won't come up at the
-# same time, though.
-jsrun -h $RUN_DIR --smpiargs="off" --nrs $NUM_NODES -e individual --stdio_stdout ${RUN_DIR}/worker_out.%j.%h.%p --stdio_stderr ${RUN_DIR}/worker_error.%j.%h.%p --tasks_per_rs 1 --cpu_per_rs 39 --gpu_per_rs 6 --rs_per_host 1 -b none --latency_priority gpu-cpu /autofs/nccs-svm1_home1/davidsonrb/Scripts/debugging/af_minimization/spawn_worker.sh &
+# Now launch ALL the dask workers simultaneously.  They won't come up at the same time, though.
+jsrun --smpiargs="off" --rs_per_host 6 --tasks_per_rs 1 --cpu_per_rs 1 --gpu_per_rs 1 --latency_priority gpu-cpu --bind none \
+	dask-worker --nthreads 1 --nprocs 1 --interface ib0 --no-dashboard --no-nanny --reconnect --scheduler-file ${SCHEDULER_FILE} &
+
+# --nrs $NUM_NODES 
 
 echo Waiting for workers
 
 # Hopefully long enough for some workers to spin up and wait for work
 sleep 30
 
-# Run the client task manager; like the scheduler, this just needs a single
-# core to noodle away on.
-jsrun  --smpiargs="off" -e individual --stdio_stdout ${RUN_DIR}/alphafold_stdout.%j.%h.%p --stdio_stderr ${RUN_DIR}/alphafold_stderr.%j.%h.%p --nrs 1 --cpu_per_rs 1 --gpu_per_rs 0 --rs_per_host 1 --latency_priority cpu-cpu -b none python3 -u /autofs/nccs-svm1_home1/davidsonrb/Scripts/debugging/af_minimization/minimization_taskmgr.py --scheduler-file $SCHEDULER_FILE --input-file /autofs/nccs-svm1_home1/davidsonrb/Scripts/debugging/af_minimization/input_file.lst
+# Run the client task manager; like the scheduler, this just needs a single core to noodle away on, which python takes naturally (no jsrun call needed)
+python3 ${RUN_DIR}/minimization_taskmgr.py --scheduler-file $SCHEDULER_FILE --input-file ${RUN_DIR}/input_file.lst 1> ${RUN_DIR}/tskmgr.stdout 2> ${RUN_DIR}/tskmgr.stderr
 
 # We're done so kill the scheduler and worker processes
 jskill all
--- Summit/minimization/minimization_taskmgr.py
+++ Summit/minimization/minimization_taskmgr.py
@@ -26,19 +26,25 @@ import time
 import argparse
 import logging
 
-from rich.console import Console
-from rich.table import Table
-from rich import print
-from rich import pretty
-pretty.install()
-from rich.traceback import install
-install()
-from rich.logging import RichHandler
-rich_handler = RichHandler(rich_tracebacks=True,
-                           markup=True)
-logging.basicConfig(level='INFO', format='%(message)s',
-                    datefmt="[%Y/%m/%d %H:%M:%S]",
-                    handlers=[rich_handler])
+import pdbfixer
+from simtk import openmm
+from simtk import unit
+from simtk.openmm import app as openmm_app
+from simtk.openmm.app.internal.pdbstructure import PdbStructure
+
+#from rich.console import Console
+#from rich.table import Table
+#from rich import print
+#from rich import pretty
+#pretty.install()
+#from rich.traceback import install
+#install()
+#from rich.logging import RichHandler
+#rich_handler = RichHandler(rich_tracebacks=True,
+#                           markup=True)
+#logging.basicConfig(level='INFO', format='%(message)s',
+#                    datefmt="[%Y/%m/%d %H:%M:%S]",
+#                    handlers=[rich_handler])
 
 from distributed import Client, as_completed
 
@@ -51,14 +57,37 @@ RELAX_EXCLUDE_RESIDUES = []     # fed to openmm minimizeEnergy; left empty by de
 ### DASK RELATED FUNCTIONS
 #######################################
 
+def setup_logger(name, log_file, level=logging.INFO):
+    """To setup as many loggers as you want"""
+    formatter = logging.Formatter('%(asctime)s      %(levelname)s       %(message)s')
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
 def get_num_workers(client):
-    """
+    """ Get the number of active workers
     :param client: active dask client
     :return: the number of workers registered to the scheduler
     """
     scheduler_info = client.scheduler_info()
 
     return len(scheduler_info['workers'].keys())
+
+
+def disconnect(client, workers_list):
+    """ Shutdown the active workers in workers_list
+    :param client: active dask client
+    :param workers_list: list of dask workers
+    """
+    client.retire_workers(workers_list, close_workers=True)
+    client.shutdown()
+
 
 def read_input_file(input_file):
     """ Read text file containing proteins to be processed
@@ -67,46 +96,168 @@ def read_input_file(input_file):
     """
     with open(input_file,'r') as file_input:
         # Each line contains the path to a AF model .pdb file
-        return list(x for x in file_input)
+        return [x.rstrip() for x in file_input]
 
 
-def run_minimization(model):
+def will_restrain(atom: openmm_app.Atom, rset: str) -> bool:
+  """Returns True if the atom will be restrained by the given restraint set."""
+
+  if rset == "non_hydrogen":
+    return atom.element.name != "hydrogen"
+  elif rset == "c_alpha":
+    return atom.name == "CA"
+
+
+def _add_restraints(
+    system,             #system: openmm.System,
+    reference_pdb,      #reference_pdb: openmm_app.PDBFile,
+    stiffness,          #stiffness: unit.Unit,
+    rset,               #rset: str,
+    exclude_residues):  #exclude_residues: Sequence[int]):
+  """Adds a harmonic potential that restrains the end-to-end distance."""
+  
+  assert rset in ["non_hydrogen", "c_alpha"]
+
+  force = openmm.CustomExternalForce("0.5 * k * ((x-x0)^2 + (y-y0)^2 + (z-z0)^2)")
+  force.addGlobalParameter("k", stiffness)
+  for p in ["x0", "y0", "z0"]:
+    force.addPerParticleParameter(p)
+
+  for i, atom in enumerate(reference_pdb.topology.atoms()):
+    if atom.residue.index in exclude_residues:
+      continue
+    if will_restrain(atom, rset):
+      force.addParticle(i, reference_pdb.positions[i])
+  system.addForce(force)
+
+
+def fix_protein(input_pdb_file, output_pdb_file = 'protonated.pdb', logger = None):
     """
     """
-    import sys
-
-    start_time = time.time()
-
-    # set up the job submit line to be used across all workers
-    args = []
-    args.append('jsrun')
-    args.append('--smpiargs="none"')
-    args.append('-n1')
-    args.append('-a1')
-    args.append('-c1')
-    args.append('-g1')
-    args.append('python3')
-    args.append('/ccs/home/davidsonrb/Scripts/debugging/af_minimization/single_minimization.py')
-    args.append(model)
-    args.append(RESTRAINT_SET)
-    args.append(RELAX_EXCLUDE_RESIDUES)
-
-    sys.stdout.write('args: ' + str(args))
-
-    completed_process = subprocess.run(args=args,text=True,capture_output=True)
+    logger.info(f'Checking model for any required fixes (missing hydrogens and other atoms, etc).')
+    logger.info(f'        Loading {input_pdb_file} to check for missing atoms and add hydrogens.')
+    fixer = pdbfixer.PDBFixer(pdbfile=open(input_pdb_file,'r'))
     
-    sys.stdout.write(completed_process.stdout)
-    sys.stdout.flush()
-    sys.stderr.write(completed_process.stderr)
-    sys.stderr.flush()
+    fixer.findMissingResidues()
+    fixer.findMissingAtoms()
+    fixer.addMissingAtoms(seed=0)
+    fixer.addMissingHydrogens()
+    logger.info(f'        Saving {output_pdb_file}.')
+    openmm_app.pdbfile.PDBFile.writeFile(fixer.topology,fixer.positions,file=open(output_pdb_file,'w'))
+    return output_pdb_file
 
-    if completed_process.returncode != 0:
-        print(f'Process returned with code'
-              f' {completed_process.returncode} for protein {protein}')
 
-    stop_time = time()
+def prep_protein(pdb_file, restraint_set = "", exclude_residues = [], forcefield = "amber99sb.xml", restraint_stiffness = 10.0, platform = 'CUDA', energy_units = unit.kilocalories_per_mole, length_units = unit.angstroms, logger = None):
+    """
+    """
+    
+    logger.info(f'Preparing the simulation engine:')
+    logger.info(f'        Loading {pdb_file} to create the OpenMM simulation object.')
+    # load pdb file into an openmm Topology and coordinates object
+    pdb = openmm_app.PDBFile(pdb_file)
 
-    return start_time, stop_time, protein
+    # set the FF and constraints objects
+    logger.info(f'        Using {forcefield}.')
+    force_field = openmm_app.ForceField(forcefield)
+    
+    # prepare the restraints/constraints for the system
+    logger.info(f'        Building HBond constraints as well as restraints on {restraint_set}.')
+    constraints = openmm_app.HBonds # NOTE: check that bond lengths are good to begin with...
+    system = force_field.createSystem(pdb.topology, constraints=constraints)
+    # prep and add restraints to the simulation system
+    stiffness = restraint_stiffness * energy_units / (length_units**2)
+    if stiffness > 0. * energy_units / (length_units**2):
+        # code up the _add_restraints function
+        _add_restraints(system, pdb, stiffness, restraint_set, exclude_residues)
+    
+    logger.info(f'        Creating the OpenMM simulation object.')
+    # required to set this for prepping the simulation object
+    integrator = openmm.LangevinIntegrator(0, 0.01, 0.0)    # hard set because we won't be using it; still necessary to define for the creation of the simulation object
+    # determine what hardware will be used to perform the calculations
+    platform = openmm.Platform.getPlatformByName(platform)
+    # prep the simulation object
+    simulation = openmm_app.Simulation(pdb.topology, system, integrator, platform)
+    # set the atom positions for the simulation's system's topology
+    simulation.context.setPositions(pdb.positions)
+    
+    return simulation
+
+
+def run_minimization(simulation,out_file_name,max_iterations = 0,energy_tolerance = 2.39,fail_attempts=100,energy_units = unit.kilocalories_per_mole,length_units = unit.angstroms, logger = None):
+    """
+    """
+    tolerance = energy_tolerance * energy_units
+    attempts = 0
+    minimized = False
+    logger.info(f'Running the minimization:')
+
+    # grab initial energies and positions
+    state = simulation.context.getState(getEnergy=True, getPositions=True)
+    einit = state.getPotentialEnergy().value_in_unit(energy_units)
+    posinit = state.getPositions(asNumpy=True).value_in_unit(length_units)
+    logger.info(f'        Starting energy: {einit} kcal mol-1')
+    
+    # attempt to minimize the structure
+    while not minimized and attempts < fail_attempts:
+        # running minimization
+        simulation.minimizeEnergy(maxIterations=max_iterations,tolerance=tolerance)
+        
+        # return energies and positions
+        state = simulation.context.getState(getEnergy=True, getPositions=True)
+        efinal = state.getPotentialEnergy().value_in_unit(ENERGY)
+        positions = state.getPositions(asNumpy=True).value_in_unit(LENGTH)
+        logger.info(f'        Final energy: {efinal} kcal mol-1')
+        
+        # saving the final structure to a pdb
+        openmm_app.pdbfile.PDBFile.writeFile(simulation.topology,positions,file=open(out_file_name + '_%02d.pdb'%(attempts-1),'w'))
+        minimized = True
+        
+        #attempts += 1
+        #try:
+        #    # running minimization
+        #    simulation.minimizeEnergy(maxIterations=max_iterations,tolerance=tolerance)
+        #    
+        #    # return energies and positions
+        #    state = simulation.context.getState(getEnergy=True, getPositions=True)
+        #    efinal = state.getPotentialEnergy().value_in_unit(ENERGY)
+        #    positions = state.getPositions(asNumpy=True).value_in_unit(LENGTH)
+        #    
+        #    # 
+        #    openmm_app.pdbfile.PDBFile.writeFile(simulation.topology,positions,file=open(out_file_name + '_%02d.pdb'%(attempts-1),'w'))
+        #    minimized = True
+        #except Exception as e:
+        #    logging.info(e)
+    
+    # update for more energy logging
+    # calculate rmsd between pos and posinit
+    logger.info(f"        dE = {efinal - einit} kcal mol^{-1}")
+    
+    if not minimized:
+        raise ValueError(f"Minimization failed after {fail_attempts} attempts.")
+
+
+def run_pipeline(pdb_file, restraint_set = 'non_hydrogen', relax_exclude_residues = []):
+    """
+    """
+    path_breakdown = pdb_file.split('/')
+    path = pdb_file.split(path_breakdown[-1])[0]   # grabbing working dir path by removing the file name
+    model_descriptor = path_breakdown[-1].split('unrelaxed_')[-1][:-4]   # grabbing a good file naming descriptor 
+    min_logger = setup_logger('minimization_logger', path + model_descriptor + '.log')  # setting up the individual run's logging file
+    
+    # load pdb file and add missing atoms (mainly hydrogens)
+    start = time.time()
+    pdb_file = fix_protein(pdb_file,output_pdb_file = path + model_descriptor + '_protonated.pdb', logger = min_logger)
+    min_logger.info(f'Finished preparing the protein model for minimization; took {time.time() - start} secs.')
+
+    # send protein into an OpenMM pipeline, preparing the protein for a simulation
+    start = time.time()
+    simulation = prep_protein(pdb_file,restraint_set, exclude_residues = relax_exclude_residues, logger = min_logger)
+    min_logger.info(f'Finished preparing the simulation engine; took {time.time() - start} secs.')
+
+    # run the minimization protocol and output minimized structure
+    start = time.time()
+    run_minimization(simulation, path + model_descriptor + '_min.pdb', logger = min_logger)
+    min_logger.info(f'Finished running the minimization calculation; took {time.time() - start} secs.')
 
 
 #######################################
@@ -121,30 +272,71 @@ if __name__ == '__main__':
     parser.add_argument('--input-file', '-i', required=True, help='file containing proteins to process')
     args = parser.parse_args()
 
-    logging.info(f'Scheduler file: {args.scheduler_file}')
-    logging.info(f'Scheduler timeout: {args.scheduler_timeout}')
-    logging.info(f'Input file: {args.input_file}')
+    # setting up the main logger
+    main_logger = setup_logger('tskmgr_logger','tskmgr.log')
+    main_logger.info('Starting dask pipeline and setting up logging.')
+    main_logger.info(f'Scheduler file: {args.scheduler_file}')
+    main_logger.info(f'Scheduler timeout: {args.scheduler_timeout}')
+    main_logger.info(f'Input file: {args.input_file}')
 
     # create list of strings pointing to pdb files to be energy minimized
     proteins = read_input_file(args.input_file)
-
-    logging.info(f'Read {len(proteins)} proteins to process.')
+    main_logger.info(f'Read {len(proteins)} proteins to process.')
 
     # starting dask client
-    with Client(scheduler_file=args.scheduler_file,timeout=args.scheduler_timeout,name='energymintaskmgr') as client:
-        logging.info(f'Starting with {get_num_workers(client)} dask workers.')
-        # setting the client task
-        task_futures = client.map(run_minimization, proteins, RESTRAINT_SET,RELAX_EXCLUDE_RESIDUES)
-        
-        # pushing tasks and watching them complete
-        ac = as_completed(task_futures)
-        for i, finished_task in enumerate(ac):
-            start_time, stop_time, protein = finished_task.result()
-            logging.info(f'{protein} processed in {(stop_time - start_time) / 60} minutes.')
-            logging.info(f'{len(proteins) - i - 1} proteins left')
-        
-        logging.info(f'Finished with {get_num_workers(client)} dask workers still active.')
+    client = Client(scheduler_file=args.scheduler_file,timeout=args.scheduler_timeout,name='energymintaskmgr')
+    main_logger.info(f'Client information: {client}')
+    NUM_WORKERS = get_num_workers(client)
+    main_logger.info(f'Starting with {NUM_WORKERS} dask workers.')
 
-    logging.info('Done.')
+    # waiting for workers...
+    wait_start = time.time()
+    client.wait_for_workers(n_workers=NUM_WORKERS)
+    main_logger.info(f'Waited for {NUM_WORKERS} workers took {time.time() - wait_start} sec')
+    workers_info = client.scheduler_info()['workers']
+    connected_workers = len(workers_info)
+    main_logger.info(f'{connected_workers} workers connected')
+
+    # do the thing.
+    #task_futures = client.map(run_hello_world,list(range(NUM_WORKERS)))
+    #task_futures = client.map(run_minimization,proteins)
+    task_futures = client.map(run_pipeline,proteins, restraint_set = RESTRAINT_SET, relax_exclude_residues = RELAX_EXCLUDE_RESIDUES) 
+
+    # gather results
+    ac = as_completed(task_futures)
+    for i, finished_task in enumerate(ac):
+        start_time, stop_time, protein = finished_task.result()
+        with open('out_%d.txt'%(i),'a') as out:
+            out.write('total time spent on this: %f'%(stop_time-start_time))
+
+        main_logger.info(f'{protein} processed in {(stop_time - start_time) / 60} minutes.')
+        main_logger.info(f'{len(proteins) - i - 1} proteins left')
+
+
+    # shutting down the cluster
+    main_logger.info(f'Shutting down the cluster')
+    workers_list = list(workers_info)
+    disconnect(client,workers_list)
+    main_logger.info('Done.')
+
+
+    #with Client(scheduler_file=args.scheduler_file,timeout=args.scheduler_timeout,name='energymintaskmgr') as client:
+    #    main_logger.info(f'Starting with {get_num_workers(client)} dask workers.')
+    #    # setting the client task
+    #    try:
+    #        task_futures = client.map(run_minimization, proteins)
+    #    except:
+    #        print('something went wrong with the task_futures lines')
+    #    
+    #    # pushing tasks and watching them complete
+    #    ac = as_completed(task_futures)
+    #    for i, finished_task in enumerate(ac):
+    #        start_time, stop_time, protein = finished_task.result()
+    #        main_logger.info(f'{protein} processed in {(stop_time - start_time) / 60} minutes.')
+    #        main_logger.info(f'{len(proteins) - i - 1} proteins left')
+    #    
+    #    main_logger.info(f'Finished with {get_num_workers(client)} dask workers still active.')
+
+    #main_logger.info('Done.')
 
 
